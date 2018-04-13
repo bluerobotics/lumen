ea401c59762705948794e3ded4b86677e9147d32
dheideman <dheideman@socal.rr.com>
2018-04-12 20:00:12 -0700

    Filter and discretize input

---
+++ Lumen-Arduino/HystRound.h
@@ -0,0 +1,49 @@
+/* Blue Robotics Lumen LED Embedded Software
+------------------------------------------------
+
+Title: Lumen LED Light Embedded Microcontroller Software
+Description: This file is used on the ATtiny45 microcontroller
+on the Lumen LED Light and controls the dimming of the light. It can be
+control by two methods, first is standard servo PWM pulse from
+1100-1900 microseconds. The second is a logic high value on the signal
+line. Additionally, a thermistor is used to sense temperature and automatically
+dim the light if it is overheating.
+
+-------------------------------
+The MIT License (MIT)
+Copyright (c) 2016 Blue Robotics Inc.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+-------------------------------*/
+#ifndef HYSTROUND
+#define HYSTROUND
+
+#include "Arduino.h"
+
+class HystRound
+{
+  public:
+    HystRound();
+    HystRound(int16_t value, float hystfactor);
+    ~HystRound();
+    int16_t hystRound(float newvalue);
+
+  private:
+    int16_t _value;
+    float   _hystfactor;
+};
+
+#endif
